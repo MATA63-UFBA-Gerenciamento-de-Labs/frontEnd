fa3a3ee24bc08835d81f0c4c259842993a5f8825
Pedro Beckhauser <pedrojbeckhauser@gmail.com>
2023-11-07 22:04:04 -0300

    feat: creat login input

---
+++ components/login_input.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { InputAdornment, TextField, TextFieldProps } from "@mui/material";
+import { Visibility, SvgIconComponent } from "@mui/icons-material";
+
+type LoginInputProps = TextFieldProps & {
+  icon: SvgIconComponent;
+};
+
+export function LoginInput(props: LoginInputProps): React.ReactElement {
+  const textFieldProps = { ...props };
+  return (
+    <TextField
+      {...textFieldProps}
+      InputProps={{
+        sx: {
+          borderRadius: "4px",
+          paddingLeft: "16px",
+          height: "40px",
+          width: "384px",
+        },
+        autoComplete: "off",
+        startAdornment: (
+          <InputAdornment position="start">
+            <props.icon />
+          </InputAdornment>
+        ),
+        endAdornment: (
+          <InputAdornment position="end" sx={{ cursor: "pointer" }}>
+            <Visibility sx={{ height: "16px", color: "#1351B4" }} />
+          </InputAdornment>
+        ),
+      }}
+      variant="outlined"
+    />
+  );
+}
