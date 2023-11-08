9d43b93b7421049ac2a2c0da6abd203248f29b57
Fernando <f.nlacerda5@gmail.com>
2023-11-08 10:22:26 -0300

    a

--- pages/components/history/history.jsx
+++ pages/components/history/history.jsx
@@ -4,8 +4,8 @@ import styles from './history.module.css'
 export default function History( {date, hour, text} ){
     return(
         <div className={styles.history}>
-            <span className={styles.mini}>{date} | {hour}</span>
-            <span className={styles.big}>{text}</span>
+            <span className={styles.mini}>{date | "" } | {hour| ""}</span>
+            <span className={styles.big}>{text| ""}</span>
         </div>
     );
 }
