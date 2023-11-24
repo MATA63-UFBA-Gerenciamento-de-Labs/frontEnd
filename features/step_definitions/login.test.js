9f79bce1bbc4345d5a3677785dbacd3133071c9a
Tiago Gordiano de Souza <tiagogordiano@gmail.com>
2023-11-24 15:48:00 -0300

    add cenario com base na planilha

--- features/step_definitions/login.test.js
+++ features/step_definitions/login.test.js
@@ -10,7 +10,7 @@ let loginPage;
 
 const customTimeout = 100000; // Tempo limite personalizado em milissegundos
 
-Given('usuario na pagina de login', async () => {
+Given('usuario esta na pagina de login', async () => {
   driver = await new Builder().forBrowser('chrome').build();
   // Definindo o tempo limite para esperar até que um elemento seja encontrado
   await driver.manage().setTimeouts({ implicit: customTimeout });
@@ -21,11 +21,34 @@ Given('usuario na pagina de login', async () => {
   await loginPage.enter_url('http://localhost:3000');
 });
 
-When('usuario tenta realizar o login', async () => {
-  await loginPage.enter_username("teste");
+When('usuario tenta o login com dados validos', async () => {
+  await loginPage.enter_username("218217788");
+  await loginPage.enter_password("teste123");
+  await loginPage.press_confirm_btn();
 });
   
 Then('usuario eh redirecionado para a homepage', async () => {
+  assert.equal(true, true);
+  await loginPage.closeBrowser();
+});
+
+When('usuario tenta o login com {string} invalido', async (dado) => {
+  let matricula = "218217788";
+  let senha = "teste123";
+  if (dado == "matricula") matricula = "teste";
+  else if (dado == "senha") senha = "t";
+  await loginPage.enter_username(matricula);
+  await loginPage.enter_password(senha);
+  await loginPage.press_confirm_btn();
+});
+
+When('usuario tenta o login com matricula inexistente', async () => {
+  await loginPage.enter_username("218217789");
+  await loginPage.enter_password("teste123");
+  await loginPage.press_confirm_btn();
+});
+
+Then('sistema mostra mensagem de erro', async () => {
   assert.equal(true, true);
   await loginPage.closeBrowser();
 });
