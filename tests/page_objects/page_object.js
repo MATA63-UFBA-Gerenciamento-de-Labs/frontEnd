c5a3d7a94f2b9b9d9c33db43abecbc3a6717bd5d
Tiago Gordiano de Souza <tiagogordiano@gmail.com>
2023-11-29 14:16:47 -0300

    add metodos para lidar com alerts

--- tests/page_objects/page_object.js
+++ tests/page_objects/page_object.js
@@ -12,6 +12,18 @@ class PageObject{
     async closeBrowser(){
         await this.driver.quit();
     }
+
+    async dismiss_alert(){
+        await this.driver.switchTo().alert().dismiss();
+    }
+
+    async accept_alert(){
+        await this.driver.switchTo().alert().accept();
+    }
+
+    async write_in_alert(txt){
+        await this.driver.switchTo().alert().sendKeys(txt);
+    }
 }
 
 module.exports = PageObject;
