92ca86842212d88ad861bba63091a2b5e56b6dbb
Pedro Beckhauser <pedrojbeckhauser@gmail.com>
2023-12-04 18:51:28 -0300

    feat: hide value in password field

--- pages/login/components/login_input.tsx
+++ pages/login/components/login_input.tsx
@@ -1,22 +1,48 @@
 import React from "react";
 import { InputAdornment, TextField, TextFieldProps } from "@mui/material";
-import { Visibility, SvgIconComponent } from "@mui/icons-material";
+import {
+  Visibility,
+  SvgIconComponent,
+  VisibilityOff,
+} from "@mui/icons-material";
 import { Lock } from "@mui/icons-material";
 
 type LoginInputProps = TextFieldProps & {
   icon: SvgIconComponent;
-  setData: (value: string | ( (prevVar: string) => string ) ) => void;
+  setData: (value: string | ((prevVar: string) => string)) => void;
+  isPassword: boolean;
 };
 
 function LoginInput(props: LoginInputProps): React.ReactElement {
-  props = { icon: props.icon || Lock,
-            setData: props.setData, 
-          };
-  
+  props = {
+    icon: props.icon || Lock,
+    setData: props.setData,
+    isPassword: props.isPassword,
+  };
+
+  const [showInputValue, setShowInputValue] = React.useState(!props.isPassword);
+
+  const toggleShowInput = () => {
+    setShowInputValue(!showInputValue);
+  };
+
+  const eyeIcon = showInputValue ? (
+    <Visibility
+      sx={{ height: "16px", color: "#1351B4" }}
+      onClick={toggleShowInput}
+    />
+  ) : (
+    <VisibilityOff
+      sx={{ height: "16px", color: "#1351B4" }}
+      onClick={toggleShowInput}
+    />
+  );
+
   const textFieldProps = { ...props };
   return (
     <TextField
       {...textFieldProps}
+      type={showInputValue ? "text" : "password"}
       InputProps={{
         sx: {
           borderRadius: "4px",
@@ -32,14 +58,14 @@ function LoginInput(props: LoginInputProps): React.ReactElement {
             <props.icon />
           </InputAdornment>
         ),
-        endAdornment: (
+        endAdornment: props.isPassword ? (
           <InputAdornment position="end" sx={{ cursor: "pointer" }}>
-            <Visibility sx={{ height: "16px", color: "#1351B4" }} />
+            {eyeIcon}
           </InputAdornment>
-        ),
+        ) : null,
       }}
       variant="outlined"
-      onChange={ e => props.setData(e.currentTarget.value)}
+      onChange={(e) => props.setData(e.currentTarget.value)}
     />
   );
 }
